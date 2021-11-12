c0741b90850c9e99ab3fc2c23740c86271091e1f
FihuraTaras <figura.taras@gmail.com>
2021-11-12 14:11:44 +0200

    first commit

---
+++ Lab 6.2.2/Lab 6.2.2.cpp
@@ -0,0 +1,51 @@
+//Lab 6.2.2
+// IT-12 Fihura Taras
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int* a, const int size, const int Low, const int High, int n)
+{
+	a[n] = Low + rand() % (High - Low + 1);
+	if (n < size - 1)
+		Create(a, size, Low, High, n + 1);
+}
+void Print(int* a, const int size, int n)
+{
+	cout << "a[" << setw(2) << n << " ] = " << setw(4) << a[n] << endl;
+	if (n < size - 1)
+		Print(a, size, n + 1);
+	else
+		cout << endl;
+}
+int Max(int* a, const int size, int n, int max)
+{
+	if (a[n] > max)
+		max = a[n];
+	if (n < size - 1)
+		return Max(a, size, n + 1, max);
+	else
+		return max;
+}
+int Min(int* a, const int size, int n, int min)
+{
+	if (a[n] < min)
+		min = a[n];
+	if (n > size - 1)
+		return Min(a, size, n + 1, min);
+	else
+		return min;
+}
+int main()
+{
+	srand((unsigned)time(NULL)); // ініціалізація генератора випадкових чисел
+	const int n = 5;
+	int a[n];
+	int Low = -10;
+	int High = 10;
+	Create(a, n, Low, High, 0);
+	Print(a, n, 0);
+	cout << "max = " << Max(a, n, 1, a[0]) << endl;
+	cout << "min = " << Min(a, n, 1, a[0]) << endl;
+	return 0;
+}
